27e60aea69be46594a4199717b03e61156417a73
Luis Esquivel <luisesquivel7f5@gmail.com>
2020-12-07 16:23:39 -0600

    SIN CADENA

--- ApiNovoLeads/ApiNovoLeads/Models/ApplicationDbContext.cs
+++ ApiNovoLeads/ApiNovoLeads/Models/ApplicationDbContext.cs
@@ -22,14 +22,7 @@ namespace ApiNovoLeads
         public virtual DbSet<TiposDeSeguimiento> TiposDeSeguimientos { get; set; }
         public virtual DbSet<Usuario> Usuarios { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("data source=192.168.210.100;initial catalog=NovoLeads;user id=sa;password=ray2010?");
-            }
-        }
+       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
